4145fa0b68fb3ea76ba0ecdcf6ee643280f0dec6
ssolunna <ssolunna29@gmail.com>
2022-09-26 19:37:41 -0500

    Display the button's number when clicked

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // JavaScript
 // CALCULATOR
 
+const DISPLAY = document.querySelector('#display');
+
+const BUTTONS = Array.from(document.querySelectorAll('.buttons > *'));
+
+const NUMBERS = BUTTONS.filter(btn => {
+  isNum = /[0-9]/;
+  return isNum.test(btn.textContent)
+});
+
 const add = (x, y) => x + y;
 
 const subtract = (x, y) => x - y;
@@ -10,3 +19,11 @@ const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
 
 const operate = (operator, x, y) => operator(x, y);
+
+const type = (button) => {
+  DISPLAY.textContent += button.textContent;
+};
+
+NUMBERS.forEach(num => {
+  num.addEventListener('click', () => type(num))
+});
